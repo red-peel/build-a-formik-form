8438454a86794470cda52103303b0d345440c2df
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-07-27 00:16:46 -0500

    Added Form Functionality

---
+++ build-a-formik-form_starter/src/App.js
@@ -0,0 +1,33 @@
+import {useFormik} from 'formik'
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: () =>{
+      alert("Login Successful");
+    },
+    validate: values =>{
+      let errors = {};
+      if(!values.email) {errors.email = "Field Required"}
+      else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+          values.email
+        )
+      ) {
+        errors.email = 'Invalid email address';
+      }
+      if(!values.password) {errors.password = "Field Required"}
+      return errors;
+      <form onSubmit={formik.handleSubmit}>
+
+      <div>Email</div>
+      <input id="emailField" name="email" type="text" onChange={formik.handleChange} value={formik.values.email}/>
+      {formik.errors.email ? <div id="emailError" style={{color:'red'}}>{formik.errors.email}</div>: null}
+      
+      <div>Password</div>
+      <input id="pswField" name="password" type="text" onChange={formik.handleChange} value={formik.values.password}></input>
+      {formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div>: null}
+      
+      <button id="submitBtn" type='submit'>Submit</button>
+      </form>
